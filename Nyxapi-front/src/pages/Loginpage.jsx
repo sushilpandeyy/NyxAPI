73c2074605ad731379d0e5fa3b604a4c7628d0dd
Sahil-Sharma06 <116885079+Sahil-Sharma06@users.noreply.github.com>
2024-10-09 11:44:32 +0530

    Changes in Login and signup component and main.jsx file

---
+++ Nyxapi-front/src/pages/Loginpage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from '../components/login'
+import Signup from '../components/signup'
+const Loginpage = () => {
+  return (
+    <div>
+        <Login/>
+        <Signup/>
+    </div>
+  )
+}
+
+export default Loginpage
